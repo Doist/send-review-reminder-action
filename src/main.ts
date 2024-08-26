719638188c3648d8115383e8c38d7a1852f9876d
Luke Merrett <luke@doist.com>
2024-08-26 13:05:36 +0100

    feat: Allowing mapping of GH usernames to Twist user ids

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ const REVIEW_TIME_MS = parseInt(core.getInput('review_time_ms', { required: true
 const IGNORE_AUTHORS = core.getInput('ignore_authors', { required: false })
 const TWIST_URL = core.getInput('twist_url', { required: true })
 const REMINDER_MESSAGE = core.getInput('message', { required: true })
+const AUTHOR_TO_TWIST_MAPPING = core.getInput('author_to_twist_mapping', { required: false })
 const IGNORE_DRAFT_PRS = core.getBooleanInput('ignore_draft_prs', { required: true })
 const IGNORE_LABELS = core.getInput('ignore_labels', { required: false })
 const IGNORE_PRS_WITH_FAILING_CHECKS = core.getBooleanInput('ignore_prs_with_failing_checks', {
@@ -24,6 +25,7 @@ const IGNORE_PRS_WITH_FAILING_CHECKS = core.getBooleanInput('ignore_prs_with_fai
 })
 
 async function run(): Promise<void> {
+    const authorToTwistMap = createAuthorToTwistMap(AUTHOR_TO_TWIST_MAPPING)
     const pullRequests = await fetchPullRequests(GITHUB_TOKEN, GITHUB_REPO_OWNER, GITHUB_REPO)
     for (const pullRequest of pullRequests) {
         if (shouldIgnore(pullRequest, IGNORE_AUTHORS, IGNORE_DRAFT_PRS, IGNORE_LABELS)) {
@@ -57,7 +59,12 @@ async function run(): Promise<void> {
         )
         if (remind) {
             core.info(`Sending reminder`)
-            const response = await sendReminder(pullRequest, REMINDER_MESSAGE, TWIST_URL)
+            const response = await sendReminder(
+                pullRequest,
+                REMINDER_MESSAGE,
+                TWIST_URL,
+                authorToTwistMap,
+            )
             const statusCode = response.message.statusCode as number
             if (statusCode >= 300) {
                 const message = response.message.statusMessage as string
@@ -66,6 +73,29 @@ async function run(): Promise<void> {
             }
         }
     }
+}
+
+/**
+ * Takes in a string in the format `username:twist_user_id,username:twist_user_id` (eg `bob:123,jane:456`)
+ * and parses it into a map of GitHub usernames to their associated Twist User IDs.
+ *
+ * @param input The string to process.
+ * @returns A map of GitHub usernames to their associated Twist User IDs.
+ */
+function createAuthorToTwistMap(input: string): { [id: string]: number } {
+    const mapping: { [id: string]: number } = {}
+
+    for (const individual of input.split(',')) {
+        const [username, twistUserID] = individual.split(':')
+
+        if (!username || !twistUserID) {
+            continue
+        }
+
+        mapping[username] = parseInt(twistUserID)
+    }
+
+    return mapping
 }
 
 run().catch((error) => core.setFailed((error as Error).message))
